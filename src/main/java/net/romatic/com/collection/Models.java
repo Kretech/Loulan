79a502a0123ac050376f8c2eb5ab7e7d85a85a7e
huiren <zhrlnt@gmail.com>
2018-03-13 00:33:38 +0800

    models.groupBy

--- src/main/java/net/romatic/com/collection/Models.java
+++ src/main/java/net/romatic/com/collection/Models.java
@@ -26,6 +26,24 @@ public class Models<M extends Model> extends ArrayList<M> {
         return dict;
     }
 
+    public <K> Map<K, Models<M>> groupBy(String column) {
+        Map<K, Models<M>> dict = new TreeMap<>();
+
+        forEach(model -> {
+            K key = model.__get(column);
+            Models<M> list = dict.get(key);
+            if (list == null) {
+                list = new Models<>();
+            }
+
+            list.add(model);
+
+            dict.put(key, list);
+        });
+
+        return dict;
+    }
+
     public <T> List<T> pluck(String column) {
         return stream().map(model -> (T) model.__get(column)).collect(Collectors.toList());
     }
